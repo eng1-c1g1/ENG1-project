f785d4237befdea0162d719b7920aa26b3acf4c0
Ivan Shestakov <108190038+TollGnoll@users.noreply.github.com>
2025-10-12 00:23:01 +0100

    Fix null reference exceptions at object creation

--- core/src/main/java/io/github/maze11/components/SpriteComponent.java
+++ core/src/main/java/io/github/maze11/components/SpriteComponent.java
@@ -8,12 +8,13 @@ import com.badlogic.gdx.math.Vector2;
 public class SpriteComponent implements Component {
 
     public Texture texture;
-    public boolean isShown;
-    public RenderLayer renderLayer;
+    public boolean isShown = true;
+    public RenderLayer renderLayer = RenderLayer.object;
 
     // The position specified here is the local position of the texture relative to the object origin
     // It should be used to create an offset between the two features
     //For characters, the feet of the character should be placed at the object origin
-    public Vector2 textureOffset;
-    public Vector2 size;
+    public Vector2 textureOffset = new Vector2();
+    public Vector2 size =  new Vector2();
+
 }
--- core/src/main/java/io/github/maze11/components/TransformComponent.java
+++ core/src/main/java/io/github/maze11/components/TransformComponent.java
@@ -4,7 +4,7 @@ import com.badlogic.ashley.core.Component;
 import com.badlogic.gdx.math.Vector2;
 
 public class TransformComponent implements Component {
-    public Vector2 position;
+    public Vector2 position = new Vector2();
     public Vector2 scale = new Vector2(1f,1f);
-    public float rotation;
+    public float rotation = 0f;
 }
