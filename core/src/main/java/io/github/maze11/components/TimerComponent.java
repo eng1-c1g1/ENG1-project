6778f0a1a34de516441a8bd7db35e7a0973bda39
Mutaz <57619853+PhobicCodes@users.noreply.github.com>
2025-10-22 14:34:15 +0100

    Create TimerComponent for countdown functionality

---
+++ core/src/main/java/io/github/maze11/components/TimerComponent.java
@@ -0,0 +1,20 @@
+package io.github.maze11.components;
+
+import com.badlogic.ashley.core.Component;
+
+
+/**
+ * Component for countdown timer funcitonality 
+ */
+public class TimerComponent implements Component{
+    public float timeRemaining; //in seconds
+    public float totalTime; // total durations
+    public boolean isRunning = true; 
+    public boolean hasExpired = false;
+
+    public TimerComponent(float durationInSeconds) {
+        this.timeRemaining = durationInSeconds;
+        this.totalTime = durationInSeconds
+    }
+    
+}
