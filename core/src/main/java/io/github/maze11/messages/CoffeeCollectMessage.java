d7e74a36848bdd623c2275d33ce71082256ee2fa
Ivan Shestakov <108190038+TollGnoll@users.noreply.github.com>
2025-10-29 14:36:30 +0000

    Generalise collectable objects to become interactable, remove unnecessary inheritance

--- core/src/main/java/io/github/maze11/messages/CoffeeCollectMessage.java
+++ core/src/main/java/io/github/maze11/messages/CoffeeCollectMessage.java
@@ -3,7 +3,11 @@ package io.github.maze11.messages;
 /**
  * Published whenever the player collects a coffee item
  */
-public class CoffeeCollectMessage extends CollectableMessage {
+public class CoffeeCollectMessage extends Message {
     public final float duration = 5f;
     public final float speedMultiplier = 2f;
+
+    public CoffeeCollectMessage() {
+        super(MessageType.COLLECT_COFFEE);
+    }
 }
