7bfe2f53b3a59758dcf75e4eecaae78831bffdde
Ivan Shestakov <108190038+TollGnoll@users.noreply.github.com>
2025-11-04 00:20:35 +0000

    Add code for exits

--- core/src/main/java/io/github/maze11/assetLoading/AssetPaths.java
+++ core/src/main/java/io/github/maze11/assetLoading/AssetPaths.java
@@ -12,15 +12,16 @@ public class AssetPaths {
 
     // Texture paths is a special case of the more general paths, created for convenience of assignment
     // This prevents needing to constantly repeat that all the entries are textures
-    public static Map<AssetId, String> texturePaths = Map.ofEntries(
+    static Map<AssetId, String> texturePaths = Map.ofEntries(
         entry(AssetId.PLAYER_TEXTURE, "player.png"),
         entry(AssetId.ORIGIN_INDICATOR, "origin_indicator.png"),
         entry(AssetId.COFFEE, "items/coffee.png"),
-        entry(AssetId.GOOSE, "player.png")
+        entry(AssetId.GOOSE, "player.png"),
+        entry(AssetId.EXIT, "player.png")
     );
 
     // This map can be used to declare paths to loaded files of any type
-    public static Map<AssetKey<?>, String> pathsWithTypes = Map.ofEntries(
+    static Map<AssetKey<?>, String> pathsWithTypes = Map.ofEntries(
         entry(new AssetKey<>(AssetId.TILEMAP, TiledMap.class), "map/default.tmx")
 
     );
--- core/src/main/java/io/github/maze11/messages/MessageType.java
+++ core/src/main/java/io/github/maze11/messages/MessageType.java
@@ -5,8 +5,9 @@ package io.github.maze11.messages;
  * This avoids unnecessary type casts and allows easy event sorting using switch statements
  */
 public enum MessageType {
-    COLLISION, 
+    COLLISION,
     TIMER_EXPIRED, // timer has reached zero
-    COLLECT_COFFEE, 
-    GOOSE_BITE
+    COLLECT_COFFEE,
+    GOOSE_BITE,
+    EXIT_MAZE
 }
