cebaff84b9f13aa7ce8bce9c933ee629ec0ba309
Ivan Shestakov <108190038+TollGnoll@users.noreply.github.com>
2025-11-09 18:33:11 +0000

    Move component creation to ComponentMaker (WIP)

--- core/src/main/java/factory/ComponentMaker.java
+++ core/src/main/java/factory/ComponentMaker.java
@@ -1,4 +1,128 @@
 package factory;
 
-public class ComponentMaker {
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.physics.box2d.*;
+import io.github.maze11.assetLoading.AssetId;
+import io.github.maze11.assetLoading.AssetLoader;
+import io.github.maze11.components.InteractableComponent;
+import io.github.maze11.components.PhysicsComponent;
+import io.github.maze11.components.SpriteComponent;
+import io.github.maze11.components.TransformComponent;
+import io.github.maze11.messages.InteractableMessage;
+import io.github.maze11.systems.physics.PhysicsSystem;
+
+/**
+ * Used by EntityMaker to add components to entities
+ */
+class ComponentMaker {
+
+    private final Engine engine;
+    private final AssetLoader assetLoader;
+
+    ComponentMaker (Engine engine, AssetLoader assetLoader) {
+        this.engine = engine;
+        this.assetLoader = assetLoader;
+    }
+
+    void addTransform(Entity entity, float x, float y, float xScale, float yScale) {
+        TransformComponent transform = engine.createComponent(TransformComponent.class);
+        transform.Initialize(x, y, xScale, yScale);
+        entity.add(transform);
+    }
+
+    void addTransform(Entity entity, float x, float y) {
+        addTransform(entity, x, y, 1f, 1f);
+    }
+
+    void addSprite(Entity entity, AssetId textureId) {
+        SpriteComponent sprite = engine.createComponent(SpriteComponent.class);
+        sprite.texture = assetLoader.get(textureId, Texture.class);
+        entity.add(sprite);
+    }
+
+    void addInteractable(Entity entity, InteractableMessage message, boolean disappearOnInteract) {
+        var interactableComponent = new InteractableComponent();
+        interactableComponent.activationMessage = message;
+        interactableComponent.disappearOnInteract = disappearOnInteract;
+        entity.add(interactableComponent);
+    }
+
+    void addBoxCollider(Entity entity, float x, float y,
+                                float width, float height,
+                                float offsetX, float offsetY,
+                                BodyDef.BodyType bodyType,
+                                boolean fixedRotation) {
+        PhysicsComponent physics = engine.createComponent(PhysicsComponent.class);
+        physics.setBox(width, height);
+
+        var world = engine.getSystem(PhysicsSystem.class).getWorld();
+        physics.body = createPhysicsBody(entity, world, physics, x, y, bodyType, fixedRotation, 0f);
+
+        entity.add(physics);
+    }
+
+    void addCircleCollider(Entity entity, float x, float y,
+                                   float radius, float xOffset, float yOffset,
+                                   BodyDef.BodyType bodyType) {
+        PhysicsComponent physics = engine.createComponent(PhysicsComponent.class);
+        physics.setCircle(radius, xOffset, yOffset);
+
+        var world = engine.getSystem(PhysicsSystem.class).getWorld();
+        physics.body = createPhysicsBody(entity, world, physics, x, y, bodyType, false, 0f);
+
+        entity.add(physics);
+    }
+
+    // Helper method to create physics body based on PhysicsComponent settings
+    private Body createPhysicsBody(Entity entity, World world, PhysicsComponent physics,
+                                   float x, float y,
+                                   BodyDef.BodyType type,
+                                   boolean fixedRotation,
+                                   float linearDamping) {
+        // Create body
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = type;
+        bodyDef.position.set(x, y);
+        bodyDef.fixedRotation = fixedRotation;
+        bodyDef.linearDamping = linearDamping;
+        Body body = world.createBody(bodyDef);
+
+        // Create shape based on collider type
+        Shape shape;
+        if (physics.getColliderType() == PhysicsComponent.ColliderType.CIRCLE) {
+            // Create circle shape
+            CircleShape circle = new CircleShape();
+            circle.setRadius(physics.getColliderRadius());
+            circle.setPosition(physics.getColliderOffset());
+            shape = circle;
+        } else {
+            // Create box shape
+            PolygonShape box = new PolygonShape();
+            box.setAsBox(
+                physics.getColliderWidth() / 2f,
+                physics.getColliderHeight() / 2f,
+                physics.getColliderOffset(),
+                0f);
+            shape = box;
+        }
+
+        // Create fixture
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = 1.0f;
+        fixtureDef.friction = 0.3f;
+        fixtureDef.restitution = 0.0f;
+
+        var fixture = body.createFixture(fixtureDef);
+
+        // Make the entity the user data so that the game can track which objects
+        // collide with which
+        fixture.setUserData(entity);
+
+        shape.dispose();
+
+        return body;
+    }
 }
--- core/src/main/java/factory/EntityMaker.java
+++ core/src/main/java/factory/EntityMaker.java
@@ -7,13 +7,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.CircleShape;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
-import com.badlogic.gdx.physics.box2d.Shape;
-import com.badlogic.gdx.physics.box2d.World;
 
 import io.github.maze11.MazeGame;
 import io.github.maze11.assetLoading.AssetId;
@@ -24,18 +18,15 @@ import io.github.maze11.components.CameraFollowComponent;
 import io.github.maze11.components.GooseComponent;
 import io.github.maze11.components.HiddenWallComponent;
 import io.github.maze11.components.InteractableComponent;
-import io.github.maze11.components.PhysicsComponent;
 import io.github.maze11.components.PlayerComponent;
 import io.github.maze11.components.SpriteComponent;
 import io.github.maze11.components.TimerComponent;
-import io.github.maze11.components.TransformComponent;
 import io.github.maze11.messages.CoffeeCollectMessage;
 import io.github.maze11.messages.GooseBiteMessage;
 import io.github.maze11.messages.InteractableMessage;
 import io.github.maze11.messages.MessageType;
 import io.github.maze11.messages.PressurePlateTriggerMessage;
 import io.github.maze11.messages.SoundMessage;
-import io.github.maze11.systems.physics.PhysicsSystem;
 
 /**
  * Used to create entities within a scene, within an engine
@@ -43,10 +34,12 @@ import io.github.maze11.systems.physics.PhysicsSystem;
 public class EntityMaker {
     private final PooledEngine engine;
     private final AssetLoader assetLoader;
+    private final ComponentMaker cMaker;
 
     public EntityMaker(PooledEngine engine, MazeGame game) {
         this.engine = engine;
         this.assetLoader = game.getAssetLoader();
+        this.cMaker = new ComponentMaker(engine, assetLoader);
     }
 
     private Entity makeEmptyEntity() {
@@ -55,35 +48,10 @@ public class EntityMaker {
         return entity;
     }
 
-    private Entity makeEntity(float x, float y, float xScale, float yScale) {
-        Entity entity = makeEmptyEntity();
-
-        TransformComponent transform = engine.createComponent(TransformComponent.class);
-        transform.Initialize(x, y, xScale, yScale);
-        entity.add(transform);
-        return entity;
-    }
-
-    private Entity makeEntity(float x, float y) {
-        return makeEntity(x, y, 1f, 1f);
-    }
-
-    private Entity makeVisibleEntity(float x, float y, float xScale, float yScale, Texture texture) {
-        Entity entity = makeEntity(x, y, xScale, yScale);
-        SpriteComponent sprite = engine.createComponent(SpriteComponent.class);
-        sprite.texture = texture;
-        entity.add(sprite);
-        return entity;
-    }
-
-    private Entity makeVisibleEntity(float x, float y, AssetId textureId) {
-        return makeVisibleEntity(x, y, 1f, 1f, assetLoader.get(textureId, Texture.class));
-    }
-
     public Entity makeFalseWall(float x, float y, float width, float height, String triggeredBy) {
         Entity entity = makeVisibleEntity(x + width / 2, y + height / 2, width, height,
                 assetLoader.get(AssetId.FALSE_WALL, Texture.class));
-        addBoxCollider(entity, x, y, width, height, BodyDef.BodyType.StaticBody, false);
+        cMaker.addBoxCollider(entity, x, y, width, height, BodyDef.BodyType.StaticBody, false);
 
         HiddenWallComponent hiddenWall = engine.createComponent(HiddenWallComponent.class);
         hiddenWall.triggeredBy = triggeredBy;
@@ -99,56 +67,14 @@ public class EntityMaker {
 
     public Entity makePressurePlate(float x, float y, String triggers) {
         Entity entity = makeInteractable(x, y, new PressurePlateTriggerMessage(triggers), true, AssetId.PRESSURE_PLATE);
-        addCircleCollider(entity, x, y, 0.75f, 0f, 0.5f, BodyDef.BodyType.StaticBody);
+        cMaker.addCircleCollider(entity, x, y, 0.75f, 0f, 0.5f, BodyDef.BodyType.StaticBody);
         return entity;
-    }
-
-    // Adds a box collider to an entity
-    private void addBoxCollider(Entity entity, float x, float y,
-            float width, float height,
-            BodyDef.BodyType bodyType,
-            boolean fixedRotation) {
-        PhysicsComponent physics = engine.createComponent(PhysicsComponent.class);
-        physics.setBox(width, height);
-
-        var world = engine.getSystem(PhysicsSystem.class).getWorld();
-        physics.body = createPhysicsBody(entity, world, physics, x, y, bodyType, fixedRotation, 0f);
-
-        entity.add(physics);
-    }
-
-    // Adds a box collider with offset to an entity
-    private void addBoxCollider(Entity entity, float x, float y,
-            float width, float height,
-            float offsetX, float offsetY,
-            BodyDef.BodyType bodyType,
-            boolean fixedRotation) {
-        PhysicsComponent physics = engine.createComponent(PhysicsComponent.class);
-        physics.setBox(width, height, offsetX, offsetY);
-
-        var world = engine.getSystem(PhysicsSystem.class).getWorld();
-        physics.body = createPhysicsBody(entity, world, physics, x, y, bodyType, fixedRotation, 0f);
-
-        entity.add(physics);
-    }
-
-    // Adds a circle collider to an entity
-    private void addCircleCollider(Entity entity, float x, float y,
-            float radius, float xOffset, float yOffset,
-            BodyDef.BodyType bodyType) {
-        PhysicsComponent physics = engine.createComponent(PhysicsComponent.class);
-        physics.setCircle(radius, xOffset, yOffset);
-
-        var world = engine.getSystem(PhysicsSystem.class).getWorld();
-        physics.body = createPhysicsBody(entity, world, physics, x, y, bodyType, false, 0f);
-
-        entity.add(physics);
     }
 
     // Creates a wall entity with box collider
     public Entity makeWall(float x, float y, float width, float height) {
         Entity entity = makeEmptyEntity();
-        addBoxCollider(entity, x + width / 2, y + height / 2, width, height,
+        cMaker.addBoxCollider(entity, x + width / 2, y + height / 2, width, height,
                 BodyDef.BodyType.StaticBody, false);
         return entity;
     }
@@ -287,77 +213,33 @@ public class EntityMaker {
         return entity;
     }
 
-    // Helper method to create physics body based on PhysicsComponent settings
-    private Body createPhysicsBody(Entity entity, World world, PhysicsComponent physics,
-            float x, float y,
-            BodyDef.BodyType type,
-            boolean fixedRotation,
-            float linearDamping) {
-        // Create body
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.type = type;
-        bodyDef.position.set(x, y);
-        bodyDef.fixedRotation = fixedRotation;
-        bodyDef.linearDamping = linearDamping;
-        Body body = world.createBody(bodyDef);
-
-        // Create shape based on collider type
-        Shape shape;
-        if (physics.getColliderType() == PhysicsComponent.ColliderType.CIRCLE) {
-            // Create circle shape
-            CircleShape circle = new CircleShape();
-            circle.setRadius(physics.getColliderRadius());
-            circle.setPosition(physics.getColliderOffset());
-            shape = circle;
-        } else {
-            // Create box shape
-            PolygonShape box = new PolygonShape();
-            box.setAsBox(
-                    physics.getColliderWidth() / 2f,
-                    physics.getColliderHeight() / 2f,
-                    physics.getColliderOffset(),
-                    0f);
-            shape = box;
-        }
-
-        // Create fixture
-        FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.shape = shape;
-        fixtureDef.density = 1.0f;
-        fixtureDef.friction = 0.3f;
-        fixtureDef.restitution = 0.0f;
-
-        var fixture = body.createFixture(fixtureDef);
-
-        // Make the entity the user data so that the game can track which objects
-        // collide with which
-        fixture.setUserData(entity);
-
-        shape.dispose();
-
-        return body;
-    }
-
     private Entity makeInteractable(float x, float y, InteractableMessage message, boolean disappearOnInteract,
             AssetId assetId) {
-        Entity entity = makeVisibleEntity(x, y, assetId);
-        var interactableComponent = new InteractableComponent();
-        interactableComponent.activationMessage = message;
-        interactableComponent.disappearOnInteract = disappearOnInteract;
-        entity.add(interactableComponent);
+        Entity entity = makeEmptyEntity();
+        cMaker.addTransform(entity, x, y);
+        cMaker.addSprite(entity, assetId);
+        cMaker.addInteractable(entity, message, disappearOnInteract);
+
         return entity;
     }
 
     public Entity makeCoffee(float x, float y) {
         Entity entity = makeInteractable(x, y, new CoffeeCollectMessage(), true, AssetId.COFFEE);
-        addCircleCollider(entity, x, y, 0.75f, 0f, 0.5f, BodyDef.BodyType.StaticBody);
+        cMaker.addCircleCollider(entity, x, y, 0.75f, 0f, 0.5f, BodyDef.BodyType.StaticBody);
         return entity;
     }
 
     public Entity makeCheckInCode(float x, float y) {
         Entity entity = makeInteractable(x, y, new InteractableMessage(MessageType.CHECK_IN_CODE_COLLECT), true,
                 AssetId.CHECK_IN);
-        addCircleCollider(entity, x, y, 0.75f, 0f, 0.5f, BodyDef.BodyType.StaticBody);
+        cMaker.addCircleCollider(entity, x, y, 0.75f, 0f, 0.5f, BodyDef.BodyType.StaticBody);
+        return entity;
+    }
+
+    public Entity makeExit(float x, float y) {
+        Entity entity = makeInteractable(x, y, new InteractableMessage(MessageType.EXIT_MAZE), false, AssetId.EXIT);
+        cMaker.addBoxCollider(entity, x, y, 1f, 1f, 0f, 0.5f,
+            BodyDef.BodyType.StaticBody, true);
         return entity;
     }
 
@@ -431,10 +313,5 @@ public class EntityMaker {
         return entity;
     }
 
-    public Entity makeExit(float x, float y) {
-        Entity entity = makeInteractable(x, y, new InteractableMessage(MessageType.EXIT_MAZE), false, AssetId.EXIT);
-        addBoxCollider(entity, x, y, 1f, 1f, 0f, 0.5f,
-                BodyDef.BodyType.StaticBody, true);
-        return entity;
-    }
+
 }
