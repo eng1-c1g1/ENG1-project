f252c4b9bb7aff187ff924219b7e18aaab2ffb97
Ivan Shestakov <108190038+TollGnoll@users.noreply.github.com>
2025-11-10 04:51:56 +0000

    Add missing documentation

--- core/src/main/java/io/github/maze11/components/HiddenWallComponent.java
+++ core/src/main/java/io/github/maze11/components/HiddenWallComponent.java
@@ -2,6 +2,9 @@ package io.github.maze11.components;
 
 import com.badlogic.ashley.core.Component;
 
+/**
+ * Represents a wall that disappears when the corresponding pressure plate is activated.
+ */
 public class HiddenWallComponent implements Component {
     /**
      * When a pressure plate with this string is triggered, the wall disappears
